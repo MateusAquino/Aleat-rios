294916ba142aa4c0546a2c13a7670e7496e61ed8
Mateus <mateusakiinoo@gmail.com>
2019-02-08 20:52:39 -0200

    Update LATIN1 Encoding Fix.js

--- LATIN1 Encoding Fix.js
+++ LATIN1 Encoding Fix.js
@@ -1,5 +1,5 @@
-function fix(wrongLatin1) {
-	return decodeURI(encodeURI(wrongLatin1).replace(/%C3%87/g, 'Ç').replace(/%C3%A7/g,'ç').replace(/(?<=%C3).+?%C2/g,""))
+function fix(wrongLatin) {
+	return decodeURI(encodeURI(wrongLatin).replace(/%C3%87/g, 'Ç').replace(/%C3%A7/g, 'ç').replace(/(?<=%C3).{1,3}%C2/g, ''))
 }
 
 console.log(fix('SÃ£o JosÃ© dos Campos')) // prints: São José dos Campos
